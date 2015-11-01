0d2f0d01df68cb13c79abf0c2e04ca12d27cd984
ishowshao <ishowshao@me.com>
2015-11-01 15:58:15 +0800

    settings form

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,19 @@
-myStock.controller('Main', function ($scope) {
-    $scope.money = 100000;
-});
+myStock.controller('Main', ['$scope', '$http', function ($scope, $http) {
+    $scope.settings = {};
+
+
+    $http.get('api/settings.php').success(function (data) {
+        console.log(data);
+        $scope.settings = data;
+    });
+
+    //$scope.$watch('money', function (newValue, oldValue) {
+    //    if (newValue && oldValue) {
+    //        console.log('money changed! need save', arguments);
+    //    }
+    //});
+
+    $scope.saveSettings = function (settings) {
+        console.log('Save', settings);
+    };
+}]);
